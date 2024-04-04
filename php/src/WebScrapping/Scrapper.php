c32a88f3874fb67b36b1428a9392fdaaf46316a8
Lucas <lsdeabre@gmail.com>
2024-04-03 23:19:28 -0300

    Criado o tratamento do HTML do Scrapper

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -16,24 +16,43 @@ class Scrapper {
   public function scrap(\DOMDocument $dom): array {
     $element = $dom->getElementById('hits-container')->parentNode;
     $result = [];
-
+    
     foreach ($element->childNodes as $child) {
+      $id; $title; $type; $authors = [];
+
       if ($child->nodeName === 'a') {
+        $link = $child->getAttribute('href');
+        $linkParts = explode('/', $link);
+        $id = (int) end($linkParts);
         
+        foreach ($child->childNodes as $elementHTML) {
+          $htmlClass = $elementHTML->getAttribute('class');
+          
+          if (str_contains($htmlClass, 'title')){
+            $title = $elementHTML->nodeValue;
+          }
+          
+          elseif (str_contains($htmlClass, 'authors')) {
+            
+            foreach ($elementHTML->childNodes as $author) {  
+              if ($author instanceof \DOMElement && $author->getAttribute('title')) {
+                $name = trim($author->nodeValue);
+                $name = str_replace(';', '', $name);
+                $institution = $author->getAttribute('title');
+                
+                $authors[] = new Person($name, $institution);
+              }
+            }
+          }
+
+          else {
+            $type = $elementHTML->firstChild->nodeValue;
+          }
+        }
+
+        $result[] = new Paper($id, $title, $type, $authors);
       }
     }
-
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    return $result;
   }
-
 }
