5e2509048fa58e8332d90beeff90ea0fc07aea92
Lucas <lsdeabre@gmail.com>
2024-04-04 13:49:33 -0300

    Adicionado algumas verificações de segurança no método scrap

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -19,40 +19,43 @@ class Scrapper {
     
     foreach ($element->childNodes as $child) {
       $id; $title; $type; $authors = [];
-
-      if ($child->nodeName === 'a') {
-        $link = $child->getAttribute('href');
-        $linkParts = explode('/', $link);
-        $id = (int) end($linkParts);
-        
-        foreach ($child->childNodes as $elementHTML) {
-          $htmlClass = $elementHTML->getAttribute('class');
+      
+      if ($child instanceof \DOMElement) {
+        if ($child->nodeName === 'a') {
+          $link = $child->hasAttribute('href') ? $child->getAttribute('href') : null;
+          $linkParts = explode('/', $link);
+          $id = (int) end($linkParts);
           
-          if (str_contains($htmlClass, 'title')){
-            $title = $elementHTML->nodeValue;
-          }
-          
-          elseif (str_contains($htmlClass, 'authors')) {
+          foreach ($child->childNodes as $elementHTML) {
+            $htmlClass = $elementHTML->getAttribute('class');
             
-            foreach ($elementHTML->childNodes as $author) {  
-              if ($author instanceof \DOMElement && $author->getAttribute('title')) {
-                $name = trim($author->nodeValue);
-                $name = str_replace(';', '', $name);
-                $institution = $author->getAttribute('title');
-                
-                $authors[] = new Person($name, $institution);
+            if (str_contains($htmlClass, 'title')){
+              $title = $elementHTML->nodeValue;
+            }
+            
+            elseif (str_contains($htmlClass, 'authors')) {
+              
+              foreach ($elementHTML->childNodes as $author) {  
+                if ($author instanceof \DOMElement && $author->getAttribute('title')) {
+                  $name = trim($author->nodeValue);
+                  $name = str_replace(';', '', $name);
+                  $institution = $author->getAttribute('title');
+                  
+                  $authors[] = new Person($name, $institution);
+                }
               }
             }
+  
+            else {
+              $type = $elementHTML->firstChild->nodeValue;
+            }
           }
-
-          else {
-            $type = $elementHTML->firstChild->nodeValue;
-          }
+  
+          $result[] = new Paper($id, $title, $type, $authors);
         }
-
-        $result[] = new Paper($id, $title, $type, $authors);
       }
     }
+    
     return $result;
   }
 }
