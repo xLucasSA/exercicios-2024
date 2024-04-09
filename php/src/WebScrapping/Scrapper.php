61ec74c187fc9b7183c22f4bc0dd0eb39a49e8b2
Lucas <lsdeabre@gmail.com>
2024-04-09 09:46:13 -0300

    Corrigido warnings do teste Scrapper, acrescentando também uma nova validação de parametros. No arquivo main foi acrescentado também o @ antes da variável  para suprimir os warnings gerados pelo arquivo HTML

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,48 +14,55 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    $element = $dom->getElementById('hits-container')->parentNode;
-    $result = [];
     
-    foreach ($element->childNodes as $child) {
-      $id; $title; $type; $authors = [];
-      
-      if ($child instanceof \DOMElement) {
-        if ($child->nodeName === 'a') {
-          $link = $child->hasAttribute('href') ? $child->getAttribute('href') : null;
-          $linkParts = explode('/', $link);
-          $id = (int) end($linkParts);
-          
-          foreach ($child->childNodes as $elementHTML) {
-            $htmlClass = $elementHTML->getAttribute('class');
+    $identifierElement = $dom->getElementById('hits-container');
+    $result = [];
+
+    if ($identifierElement) {
+      $mainElement = $identifierElement->parentNode;
+
+      foreach ($mainElement->childNodes as $child) {
+        $id; $title; $type; $authors = [];
+        
+        if ($child instanceof \DOMElement) {
+          if ($child->nodeName === 'a') {
+            $link = $child->hasAttribute('href') ? $child->getAttribute('href') : null;
+            $linkParts = explode('/', $link);
+            $id = (int) end($linkParts);
             
-            if (str_contains($htmlClass, 'title')){
-              $title = $elementHTML->nodeValue;
-            }
-            
-            elseif (str_contains($htmlClass, 'authors')) {
-              
-              foreach ($elementHTML->childNodes as $author) {  
-                if ($author instanceof \DOMElement && $author->getAttribute('title')) {
-                  $name = trim($author->nodeValue);
-                  $name = str_replace(';', '', $name);
-                  $institution = $author->getAttribute('title');
+            foreach ($child->childNodes as $elementHTML) {
+              if ($elementHTML instanceof \DOMElement) {
+                $htmlClass = $elementHTML->getAttribute('class');
+                
+                if (str_contains($htmlClass, 'title')){
+                  $title = $elementHTML->nodeValue;
+                }
+                
+                elseif (str_contains($htmlClass, 'authors')) {
                   
-                  $authors[] = new Person($name, $institution);
+                  foreach ($elementHTML->childNodes as $author) {  
+                    if ($author instanceof \DOMElement && $author->getAttribute('title')) {
+                      $name = trim($author->nodeValue);
+                      $name = str_replace(';', '', $name);
+                      $institution = $author->getAttribute('title');
+                      
+                      $authors[] = new Person($name, $institution);
+                    }
+                  }
+                }
+  
+                else {
+                  $type = $elementHTML->firstChild->nodeValue;
                 }
               }
             }
-  
-            else {
-              $type = $elementHTML->firstChild->nodeValue;
-            }
+    
+            $result[] = new Paper($id, $title, $type, $authors);
           }
-  
-          $result[] = new Paper($id, $title, $type, $authors);
         }
       }
     }
-    
+
     return $result;
   }
 }
